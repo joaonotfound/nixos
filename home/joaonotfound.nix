043e6010795ded5d6c0d1495267ec82fe1d1b9d1
joaonotfound <joaonotfound@protonmail.com>
2023-09-24 13:08:42 -0300

    Moved git config to the development.nix file

--- home/joaonotfound.nix
+++ home/joaonotfound.nix
@@ -51,28 +51,6 @@
     nixpkgs-fmt
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "joaonotfound";
-    userEmail = "joaonotfound@protonmail.com";
-
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      df = "diff";
-      lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
-      up = "!f() { git pull --rebase --autostash \"$@\"; }; f";
-      out = "push origin HEAD";
-      cob = "checkout -b";
-      uncommit = "!f() { git reset HEAD~1 --soft; }; f";
-      cleanup = "!f() { git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d; }; f";
-    };
-  };
-
-
   # Activate fonts like Jetbrains 
   fonts.fontconfig.enable = true;
 
