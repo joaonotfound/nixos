5f2c82f76390a81bc5a0eff2356106f30248b89e
joaonotfound <joaonotfound@protonmail.com>
2024-07-23 19:56:43 -0300

    Automatically starts neofetch when spawning a new shell and added more aliases

--- modules/shells/fish.nix
+++ modules/shells/fish.nix
@@ -2,15 +2,26 @@
   home.packages = with pkgs; [ 
     fish
     zoxide
+		neofetch
   ];
+
+	home.file = {
+		/* Neofetch config	 */	
+		".config/neofetch".source = ../../resources/dotfiles/neofetch;
+	};
 
   programs.fish = {
     enable = true;
-    shellInit = ''zoxide init fish | source'';
+    shellInit = ''
+		zoxide init fish | source
+		neofetch
+		'';
 		shellAliases = {
 			v = "nvim";
 			vc = "veracrypt";
 			c = "code";
+			cl = "clear";
+			l = "ls -lha";
 			p = ''fish -P --init-command "function fish_prompt
         set_color green
         echo -n (whoami)@\'\'
