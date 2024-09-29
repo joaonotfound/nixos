7a43a4d6affcf9cf63e1fc41d22a47b3dece487a
joaonotfound <joaonotfound@protonmail.com>
2024-09-28 21:51:45 -0300

    backup files when updating home manager

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+						home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = {
               inherit inputs;
               inherit pkgs;
